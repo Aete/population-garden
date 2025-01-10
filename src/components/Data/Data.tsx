a4398317c9c0f9defb486b3f8adb6b436921916f
Seunggyun <hsg8234@naver.com>
2025-01-10 22:01:45 +0900

    prototyping the data table but there is an issue for the webpage rendering

--- src/components/Data/Data.tsx
+++ src/components/Data/Data.tsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { tablet } from "../../utils/style";
 import { ChapterTitle, ChapterText, ChapterTextKR } from "../common/text";
+import Table from "./Table";
+import { useEffect, useState } from "react";
 
 const Description = styled.div`
   grid-column: 1 / span 5;
@@ -24,23 +26,58 @@ const Container = styled.div`
 `;
 
 export default function Data(): JSX.Element {
+  const [isTabletOrSmaller, setIsTabletOrSmaller] = useState(
+    window.innerWidth <= tablet
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsTabletOrSmaller(window.innerWidth <= tablet);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  if (isTabletOrSmaller) {
+    return (
+      <Container>
+        <Description>
+          <ChapterTitle>Data</ChapterTitle>
+          <Table numRows={3} />
+          <DataText />
+        </Description>
+      </Container>
+    );
+  } else {
+    return (
+      <Container>
+        <Description>
+          <ChapterTitle>Data</ChapterTitle>
+          <DataText />
+        </Description>
+        <Table numRows={7} />
+      </Container>
+    );
+  }
+}
+
+function DataText(): JSX.Element {
   return (
-    <Container>
-      <Description>
-        <ChapterTitle>Data</ChapterTitle>
-        <ChapterText>
-          Seoul's Living Population data is an estimated dataset of the
-          population present in specific areas and time points in Seoul, based
-          on mobile communication data (LTE) and utilizing public transportation
-          usage statistics, resident registration population statistics,
-          building databases and etc.
-        </ChapterText>
-        <ChapterTextKR>
-          서울시 생활인구는 모바일 통신 데이터 (LTE)를 기반으로 대중교통
-          이용통계, 주민등록인구통계, 건물 DB 등을 활용하여 서울의 특정지역,
-          시점에 존재하는 인구를 추정한 데이터입니다.
-        </ChapterTextKR>
-      </Description>
-    </Container>
+    <>
+      <ChapterText>
+        Seoul's Living Population data is an estimated dataset of the population
+        present in specific areas and time points in Seoul, based on mobile
+        communication data (LTE) and utilizing public transportation usage
+        statistics, resident registration population statistics, building
+        databases and etc.
+      </ChapterText>
+      <ChapterTextKR>
+        서울시 생활인구는 모바일 통신 데이터 (LTE)를 기반으로 대중교통 이용통계,
+        주민등록인구통계, 건물 DB 등을 활용하여 서울의 특정지역, 시점에 존재하는
+        인구를 추정한 데이터입니다.
+      </ChapterTextKR>
+    </>
   );
 }
---
+++ src/components/Data/Table.tsx
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+import { tablet } from "../../utils/style";
+import data from "../../utils/2022_monthly.json";
+
+const TableContainer = styled.div`
+  background-color: #212121;
+  padding: 20px;
+  border-radius: 8px;
+  grid-column: 7 / span 6;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  @media screen and (max-width: ${tablet}px) {
+    width: 90%;
+    margin: 40px auto;
+  }
+`;
+
+const StyledTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  color: white;
+`;
+
+const StyledTh = styled.th`
+  border-bottom: 2px solid #969696;
+  padding: 10px;
+  text-align: left;
+  vertical-align: middle;
+`;
+
+const StyledTd = styled.td`
+  border-bottom: 1px solid #444;
+  padding: 10px;
+  font-weight: 200;
+`;
+
+const StyleThRight = styled(StyledTh)`
+  text-align: right;
+`;
+
+const StyleTdRight = styled(StyledTd)`
+  text-align: right;
+`;
+
+interface TableProps {
+  numRows: number;
+}
+
+export default function Table({ numRows }: TableProps): JSX.Element {
+  return (
+    <TableContainer>
+      <StyledTable>
+        <thead>
+          <tr>
+            <StyledTh>Gu Code</StyledTh>
+            <StyleThRight>Month</StyleThRight>
+            <StyleThRight>Total Population</StyleThRight>
+            <StyleThRight>Male Populaton</StyleThRight>
+            <StyleThRight>Female Population</StyleThRight>
+            <StyleThRight>Female Ratio</StyleThRight>
+          </tr>
+        </thead>
+        <tbody>
+          {data.slice(0, numRows).map((item) => {
+            return (
+              <tr key={`${item.gu}_${item.month}`}>
+                <StyledTd>{item.gu}</StyledTd>
+                <StyleTdRight>{item.month}</StyleTdRight>
+                <StyleTdRight>
+                  {Math.round(item.data[0][0]).toLocaleString()}
+                </StyleTdRight>
+                <StyleTdRight>
+                  {Math.round(item.data[0][1]).toLocaleString()}
+                </StyleTdRight>
+                <StyleTdRight>
+                  {Math.round(item.data[0][2]).toLocaleString()}
+                </StyleTdRight>
+                <StyleTdRight>
+                  {Math.round((1 - item.data[0][3]) * 100) / 100}
+                </StyleTdRight>
+              </tr>
+            );
+          })}
+        </tbody>
+      </StyledTable>
+    </TableContainer>
+  );
+}
